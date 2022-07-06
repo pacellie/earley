b97dfd26a82b49bcea32227f68868ae66a0ef2a3
Martin Rau <martin.rau@tum.de>
2022-07-06 22:26:59 +0200

    prepare for meeting

--- Examples.thy
+++ Examples.thy
@@ -2,15 +2,17 @@ theory Examples
   imports Earley_List
 begin
 
-(*
-typedecl symbol
+subsection \<open>Problem 1\<close>
+
+(* CFG.thy
+typedecl symbol <-- HERE
 
 type_synonym rule = "symbol \<times> symbol list"
 
 type_synonym sentence = "symbol list"
 
 locale CFG =
-  fixes \<NN> :: "symbol set"
+  fixes \<NN> :: "symbol set" <-- use "'a set" instead everywhere?
   fixes \<TT> :: "symbol set"
   fixes \<RR> :: "rule set"
   fixes \<SS> :: "symbol"
@@ -28,9 +30,21 @@ definition grammar :: "rule set" where
     (Nonterminal S, [Nonterminal S, Terminal plus, Nonterminal S])
   }"
 
+text\<open>
+  Solution: remove `symbol` instead use "'a" everywhere? But then we can't depend on Steven's
+  AFP entry except for very few things and have to copy/rewrite CFG, ... files.
+\<close>
+
+text\<open>
+  Follow up question: How do we want to specify a grammar? Like above if with typedecl, or ...?
+\<close>
+
+
+subsection \<open>Problem 2\<close>
+
 (*
 locale Earley_Set = CFG +
-  fixes inp :: "symbol list"
+  fixes inp :: "symbol list" <-- HERE
   assumes valid_inp: "set inp \<subseteq> \<TT>"
   assumes finite: "finite \<RR>"
 *)
@@ -43,15 +57,29 @@ fun f :: "'a \<Rightarrow> nat" where
 
 end
 
-global_interpretation test: Test "5" .
+global_interpretation test: Test "5::nat"
+  defines f_impl = test.f .
 
-term test.f
+term f_impl
+value "f_impl (0::nat)"
+
+text\<open>
+  Solution: remove `inp` from locale and pass it everywhere as an additional argument?
+\<close>
+
+
+subsection \<open>'Problem'/TODO 3\<close>
 
 (*
 locale Earley_List = Earley_Set +
   fixes rules :: "rule list"
   assumes valid_rules: "set rules = \<RR> \<and> distinct rules"
-  assumes nonempty_deriv: "N \<in> \<NN> \<Longrightarrow> \<not> derives [N] []"
+  assumes nonempty_deriv: "N \<in> \<NN> \<Longrightarrow> \<not> derives [N] []" <-- HERE
 *)
+
+text\<open>
+  Write a function (naive/more involved) that given a grammar and a nonterminal
+  checks if this terminal has an empty derivation then prove by `eval`.
+\<close>
 
 end
